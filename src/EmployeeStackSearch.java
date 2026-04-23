d28844e668e25c4dfc7969cc0690219429ff0c31
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-23 12:09:30 +0400

    Create stack and add employee action records

--- src/EmployeeStackSearch.java
+++ src/EmployeeStackSearch.java
@@ -3,6 +3,12 @@ import java.util.Stack;
 
 public class EmployeeStackSearch {
     public static void main(String[] args) {
-
+        Stack<String> workSearch = new Stack<>();
+        workSearch.push("Ahmed logged into the HR portal");
+        workSearch.push("Mona uploaded attendance report");
+        workSearch.push("Salim approved leave request");
+        workSearch.push("Huda updated salary details");
+        workSearch.push("Yasir checked warehouse stock");
+        workSearch.push("Reem sent invoice to client");
     }
 }
