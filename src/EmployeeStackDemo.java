08394aab83b97c2062323c64033dc19c71644b51
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-23 11:51:27 +0400

    Add first employee actions using push

--- src/EmployeeStackDemo.java
+++ src/EmployeeStackDemo.java
@@ -3,5 +3,10 @@ import java.util.Stack;
 public class EmployeeStackDemo {
     public static void main (String[] args){
         Stack<String> actionLog = new Stack<>();
+        actionLog.push("Ali logged into the system");
+        actionLog.push("Sara updated customer details");
+        actionLog.push("Maha approved the invoice");
+        actionLog.push("Hamed checked stock items");
+        actionLog.push("Noor generated the daily report");
     }
 }
