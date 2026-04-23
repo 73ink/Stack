e342627b867d6c139f8c56efc03caf34b0800ee7
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-23 12:32:53 +0400

    Visit initial pages and show history after each visit

--- src/BrowserHistorySimulator.java
+++ src/BrowserHistorySimulator.java
@@ -4,5 +4,14 @@ public class BrowserHistorySimulator {
     public static void main (String[] args){
         Stack<String> pageTrail = new Stack<>();
 
+        visitPage(pageTrail, "https://www.figma.com");
+        visitPage(pageTrail, "https://www.quillbot.com");
+        visitPage(pageTrail, "https://www.github.com");
+        visitPage(pageTrail, "https://www.translate.google.com");
+        visitPage(pageTrail, "https://www.typingtest.com");
+
+    }
+    public static void visitPage(Stack<String> pageTrail, String url) {
+
     }
 }
