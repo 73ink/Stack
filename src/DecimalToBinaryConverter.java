2933c7f10251df270791dd33a1f1cd5b969c673d
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-26 12:01:18 +0400

    Add decimal numbers for testing, and decimalToBinary method

--- src/DecimalToBinaryConverter.java
+++ src/DecimalToBinaryConverter.java
@@ -1,7 +1,17 @@
 import java.util.Stack;
 
 public class DecimalToBinaryConverter {
-    public static void main (String[] args){
-
+    public static void main (String[] args) {
+        int[] numbers = {0, 1, 5, 10, 15, 25}; // 6 different decimal numbers to convert to binary
+        // output prints based on the case
+        for (int number : numbers) {
+            System.out.println("Decimal number: " + number);
+            System.out.println("Binary number: " + decimalToBinary(number));
+            System.out.println("--------------------");
+        }
     }
+        public static String decimalToBinary(int num){
+        return "0";
+        }
+
 }
