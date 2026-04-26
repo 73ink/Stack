71695e294d42f594d2d6d6e5e74cf80aeee3cead
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-26 12:15:04 +0400

    Convert decimal to binary using stack, add steps comment to follow the flow of operation

--- src/DecimalToBinaryConverter.java
+++ src/DecimalToBinaryConverter.java
@@ -1,17 +1,40 @@
 import java.util.Stack;
 
 public class DecimalToBinaryConverter {
-    public static void main (String[] args) {
-        int[] numbers = {0, 1, 5, 10, 15, 25}; // 6 different decimal numbers to convert to binary
+    public static void main(String[] args) {
+        int[] nums = {0, 1, 5, 10, 15, 25}; // 6 different decimal numbers to convert to binary
         // output prints based on the case
-        for (int number : numbers) {
-            System.out.println("Decimal number: " + number);
-            System.out.println("Binary number: " + decimalToBinary(number));
+        for (int num : nums) {
+            System.out.println("Decimal number: " + num);
+            System.out.println("Binary number: " + decimalToBinary(num));
             System.out.println("--------------------");
         }
     }
-        public static String decimalToBinary(int num){
-        return "0";
-        }
 
+    public static String decimalToBinary(int num) {
+        // if number is 0, binary is also 0
+        if (num == 0) {
+            return "0";
+        }
+        // create a stack to store remainders
+        Stack<Integer> remainders = new Stack<>();
+        // keep dividing the number by 2
+        while (num >0){
+            // get remainder 0 or 1
+            int reminder = num % 2;
+            // push remainder into stack
+            remainders.push(reminder);
+            // update number divide by 2
+            num = num /2;
+        }
+        // create empty string to store final binary result
+        String binary ="";
+        // pop all elements from stack which will reverses the order
+        while (!remainders.isEmpty()){
+            binary = binary + remainders.pop();
+        }
+        // finally, return final binary string
+        return binary;
+
+    }
 }
