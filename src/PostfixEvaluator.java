dbbe971230b087fe3317ce3fa7b7ffe575564023
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-26 12:42:58 +0400

    Return final postfix result after validation

--- src/PostfixEvaluator.java
+++ src/PostfixEvaluator.java
@@ -11,9 +11,33 @@ public class PostfixEvaluator {
             System.out.println("--------------------");
         }
     }
-    public static String evaluatePostfix(String expression){
-        Stack<Integer> numbers = new Stack<>();
-            return () ;
+    // replaced String with int
+    public static int evaluatePostfix(String exp){
+        Stack<Integer> nums = new Stack<>();
+        // Show token by token evaluation steps
+        for (int i = 0; i < exp.length(); i++) {
+            char ch = exp.charAt(i);
+
+            if (ch >= '0' && ch <= '9') {
+                nums.push(ch - '0');
+            } else {
+                int second = nums.pop();
+                int first = nums.pop();
+
+                if (ch == '+') {
+                    nums.push(first + second);
+                } else if (ch == '-') {
+                    nums.push(first - second);
+                } else if (ch == '*') {
+                    nums.push(first * second);
+                } else if (ch == '/') {
+                    nums.push(first / second);
+                } else if (ch == '%') {
+                    nums.push(first % second);
+                }
+            }
+        }
+        return nums.pop();
 
     }
 }
