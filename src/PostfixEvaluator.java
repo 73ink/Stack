5473f1db0ba1a89e73d43281f5dfadaf385d9cb6
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-26 12:32:13 +0400

    Evaluate all postfix expressions

--- src/PostfixEvaluator.java
+++ src/PostfixEvaluator.java
@@ -5,6 +5,11 @@ public class PostfixEvaluator {
         // 6 different postfix expressions to evaluate
         String[] expressions = {"34+", "52-", "62*", "82/", "93%", "23+4*"};
 
+        for (String exp : expressions) {
+            System.out.println("Postfix expression: " + exp);
+            System.out.println("Result: " + evaluatePostfix(exp));
+            System.out.println("--------------------");
+        }
     }
     public static String evaluatePostfix(String expression){
 
