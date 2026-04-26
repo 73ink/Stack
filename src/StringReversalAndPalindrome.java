883d4798fe530fe6173622f7f4d09c4b3981d6d8
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-26 10:58:05 +0400

    Task 6 was created, class, main method, string method, and imports was added

--- src/StringReversalAndPalindrome.java
+++ src/StringReversalAndPalindrome.java
@@ -1,2 +1,12 @@
+import java.util.Stack;
+
 public class StringReversalAndPalindrome {
+    public static void main (String[] args){
+
+
+    }
+    public static String reverseString(String input){
+        return x;
+
+    }
 }
