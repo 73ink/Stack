d385f55ff53d79e2ced9cd230f6c27621089a17b
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-26 11:03:25 +0400

    Add simple strings for testing

--- src/StringReversalAndPalindrome.java
+++ src/StringReversalAndPalindrome.java
@@ -2,11 +2,27 @@ import java.util.Stack;
 
 public class StringReversalAndPalindrome {
     public static void main (String[] args){
+        // Add simple strings for testing
+        String[] words = {"app", "hallah","mom", "level", "RaceCar", "", "a"};
+            // output and system prints
+        for (String word : words) {
+            System.out.println("Original word: " + word);
 
+            String reversed = reverseString(word);
+            System.out.println("Reversed word: " + reversed);
+
+            boolean result = isPalindrome(word);
+            System.out.println("Is palindrome? " + result);
+
+            System.out.println("--------------------");
+        }
 
     }
-    public static String reverseString(String input){
+    public static String reverseString(String word){
         return x;
 
+    }
+    public static boolean isPalindrome(String word){
+        return y;
     }
 }
