568ce62a62bccb549923ad0f333c16615e35f947
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-26 15:42:38 +0400

    Type some text in the editor

--- src/TextEditorWithUndoRedo.java
+++ src/TextEditorWithUndoRedo.java
@@ -1,7 +1,19 @@
 import java.util.Stack;
 
 public class TextEditorWithUndoRedo {
+    static Stack<String> undoStack = new Stack<>();
+    static Stack<String> redoStack = new Stack<>();
+    static String text = "";
     public static void main (String[] args){
+        type("Hello");
+        type(" World");
+        type(" Java");
+
+
 
     }
+    public static void type(String newText){
+
+    }
+    
 }
