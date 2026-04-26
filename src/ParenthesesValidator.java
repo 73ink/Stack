6005a9a80ce56438af9f56e278d91e99856522a0
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-26 09:03:06 +0400

    Create test expressions for bracket validation

--- src/ParenthesesValidator.java
+++ src/ParenthesesValidator.java
@@ -1,6 +1,13 @@
 import java.util.Stack;
 public class ParenthesesValidator {
     public static void main(String[] args){
+        String[] sampleLines = {
+         "{[()]}",
+         "((()))",
+         "[[",
+         ")))",
+          "",
+        };
 
     }
 }
