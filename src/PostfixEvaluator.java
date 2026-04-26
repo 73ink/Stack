a245349ef73abbb8a57bb112f452972a9d20490a
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-26 12:46:37 +0400

    Task 8 was completed and tested

--- src/PostfixEvaluator.java
+++ src/PostfixEvaluator.java
@@ -3,7 +3,7 @@ import java.util.Stack;
 public class PostfixEvaluator {
     public static void main(String[] args) {
         // 6 different postfix expressions to evaluate
-        String[] expressions = {"34+", "52-", "62*", "82/", "93%", "23+4*"};
+        String[] expressions = {"34+", "52-", "62*", "82-4/", "93%", "23+4*"};
 
         for (String exp : expressions) {
             System.out.println("Postfix expression: " + exp);
