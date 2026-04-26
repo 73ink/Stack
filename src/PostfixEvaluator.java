3c05e59db9eca4ba5cf3c1259e803cf3d3c84b4a
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-04-26 12:31:22 +0400

    Create postfix test expressions

--- src/PostfixEvaluator.java
+++ src/PostfixEvaluator.java
@@ -2,6 +2,11 @@ import java.util.Stack;
 
 public class PostfixEvaluator {
     public static void main(String[] args) {
+        // 6 different postfix expressions to evaluate
+        String[] expressions = {"34+", "52-", "62*", "82/", "93%", "23+4*"};
+
+    }
+    public static String evaluatePostfix(String expression){
 
     }
 }
