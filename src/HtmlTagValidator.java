f9f6e85c56bf676907e73ad516f0cb7142783fc9
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-05-11 19:44:32 +0400

    Add simple tag examples

--- src/HtmlTagValidator.java
+++ src/HtmlTagValidator.java
@@ -4,5 +4,20 @@ public class HtmlTagValidator {
 
     public static void main(String[] args){
 
+        String[] tags ={
+                "<b><i></i></b>",
+                "<p></p>",
+                "<b><i></b></i>",
+                "<h1></h1>",
+                "<div><p></p></div>"
+        };
+
+        for (String html : tags){
+            System.out.println("HTML: " + html);
+            System.out.println("Valid? " + checkTags(html));
+            System.out.println("---------------");
+        }
+
     }
+
 }
