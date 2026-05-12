41ff31872894c504c7262fe748b7776e13a9b1bd
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-05-12 22:05:13 +0400

    implement all the methods required

--- src/TowerOfHanoiSolver.java
+++ src/TowerOfHanoiSolver.java
@@ -5,7 +5,31 @@ public class TowerOfHanoiSolver {
     static Stack<Integer> pegA = new Stack<>();
     static Stack<Integer> pegB = new Stack<>();
     static Stack<Integer> pegC = new Stack<>();
+
+    static int moves = 0;
+
     public static void main (String[] args){
 
     }
+
+    public static void runHanoi(int numDisks){
+
+    }
+    public static void initializePegs(int numDisks){
+
+    }
+    public static void solveHanoi(int n, char from, char to, char aux){
+
+    }
+    public static void moveDisk(char fromPeg, char toPeg) {
+
+    }
+    public static boolean isValidMove(char fromPeg, char toPeg) {
+
+    return false;
+    }
+    public static void displayPegs() {
+
+    }
+
 }
